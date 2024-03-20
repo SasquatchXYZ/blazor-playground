0c83a52f1fe0b17e33e9225fede81963f0c579d9
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-03-20 17:22:58 -0400

    add OrderDetail page and redirect there after placing order

--- BlazingPizza/Controllers/OrderController.cs
+++ BlazingPizza/Controllers/OrderController.cs
@@ -47,4 +47,21 @@ public class OrderController : Controller
 
         return order.OrderId;
     }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order is null)
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
+    }
 }
