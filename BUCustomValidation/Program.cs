ca08a16bb546fbdde1ac316d8589ae41954b6b4c
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-03-31 17:09:22 -0400

    register validator

--- BUCustomValidation/Program.cs
+++ BUCustomValidation/Program.cs
@@ -1,10 +1,12 @@
 using BUCustomValidation.Components;
+using BUCustomValidation.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<PersonValidator>();
 
 var app = builder.Build();
 
