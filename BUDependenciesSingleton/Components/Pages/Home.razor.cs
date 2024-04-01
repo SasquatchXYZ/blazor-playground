23de2e653845daf1e18d7f673e0c3b185cb307bb
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-01 19:05:56 -0400

    consuming IChatService

--- BUDependenciesSingleton/Components/Pages/Home.razor.cs
+++ BUDependenciesSingleton/Components/Pages/Home.razor.cs
@@ -1,12 +1,41 @@
 using System.ComponentModel.DataAnnotations;
+using BUDependenciesSingleton.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace BUDependenciesSingleton.Components.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Required(ErrorMessage = "Enter name")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "Enter a message")]
     public string Text { get; set; }
+
+    [Inject] private IChatService ChatService { get; set; }
+
+    private List<string>? ChatWindowText => ChatService.ChatWindowText;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        ChatService.TextAdded += TextAdded;
+    }
+
+    private void SendMessage()
+    {
+        if (ChatService.SendMessage(Name, Text))
+            Text = "";
+    }
+
+    private void TextAdded(object? sender, EventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        ChatService.TextAdded -= TextAdded;
+        GC.SuppressFinalize(this);
+    }
 }
