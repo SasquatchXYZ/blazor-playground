e71aa0ae9ba36aeae62fc77b403dfa62f5dff143
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-03-31 23:08:53 -0400

    add callback so that we aren't attempting to invoke methods on a disposed .NET object

--- BUCallingDotnetFromJs/wwwroot/scripts/CallingDotNetFromJavaScript.js
+++ BUCallingDotnetFromJs/wwwroot/scripts/CallingDotNetFromJavaScript.js
@@ -1,8 +1,11 @@
 var BlazorUniversity = BlazorUniversity || {};
 BlazorUniversity.startRandomGenerator = function (dotNetObject) {
-    setInterval(function () {
+    return setInterval(function () {
         let text = Math.random() * 1000;
         console.log("JS: Generated " + text);
         dotNetObject.invokeMethodAsync('AddText', text.toString());
     }, 1000);
+};
+BlazorUniversity.stopRandomGenerator = function (handle) {
+    clearInterval(handle)
 };
