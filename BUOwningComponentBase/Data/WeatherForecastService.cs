2afd5571590d4cad236abfb039381ee1116eb525
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-01 23:46:24 -0400

    change WeatherForecastService to singleton and update to have locks for simulating errors

--- BUOwningComponentBase/Data/WeatherForecastService.cs
+++ BUOwningComponentBase/Data/WeatherForecastService.cs
@@ -2,6 +2,8 @@ namespace BUOwningComponentBase.Data;
 
 public class WeatherForecastService
 {
+    private volatile int Locked;
+
     private static readonly string[] _summaries =
     [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -9,16 +11,26 @@ public class WeatherForecastService
 
     public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
-        // Simulate asynchronous loading to demonstrate streaming rendering
-        await Task.Delay(500);
+        if (Interlocked.CompareExchange(ref Locked, 1, 0) > 0)
+            throw new InvalidOperationException(
+                "A second operation started on this context before a previous operation completed.  Any"
+                + "instance members are not guaranteed to be thread-safe.");
 
-        var startDateOnly = DateOnly.FromDateTime(startDate);
-
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        try
         {
-            Date = startDateOnly.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = _summaries[Random.Shared.Next(_summaries.Length)]
-        }).ToArray();
+            // Simulate asynchronous loading to demonstrate streaming rendering
+            await Task.Delay(3000);
+            var startDateOnly = DateOnly.FromDateTime(startDate);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDateOnly.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+            }).ToArray();
+        }
+        finally
+        {
+            Interlocked.Decrement(ref Locked);
+        }
     }
 }
--- BUOwningComponentBase/Program.cs
+++ BUOwningComponentBase/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped<WeatherForecastService>();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
