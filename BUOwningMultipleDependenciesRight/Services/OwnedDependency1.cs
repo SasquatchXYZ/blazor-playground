598e61fe012899667b40091efae34287f47b492d
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-02 23:19:01 -0400

    properly dispose of the components and add logs to demonstrate this

--- BUOwningMultipleDependenciesRight/Services/OwnedDependency1.cs
+++ BUOwningMultipleDependenciesRight/Services/OwnedDependency1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace BUOwningMultipleDependenciesRight.Services;
 
 public interface IOwnedDependency1
@@ -5,7 +7,7 @@ public interface IOwnedDependency1
     public int InstanceNumber { get; }
 }
 
-public class OwnedDependency1 : IOwnedDependency1
+public class OwnedDependency1 : IOwnedDependency1, IDisposable
 {
     private static volatile int _previousInstanceNumber;
 
@@ -14,5 +16,12 @@ public class OwnedDependency1 : IOwnedDependency1
     public OwnedDependency1()
     {
         InstanceNumber = Interlocked.Increment(ref _previousInstanceNumber);
+        Debug.WriteLine($"Created {GetType().Name} instance {InstanceNumber}");
+    }
+
+    public void Dispose()
+    {
+        Debug.WriteLine($"Disposing {GetType().Name} instance {InstanceNumber}");
+        GC.SuppressFinalize(this);
     }
 }
--- BUOwningMultipleDependenciesRight/Services/OwnedDependency2.cs
+++ BUOwningMultipleDependenciesRight/Services/OwnedDependency2.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace BUOwningMultipleDependenciesRight.Services;
 
 public interface IOwnedDependency2
@@ -5,7 +7,7 @@ public interface IOwnedDependency2
     public int InstanceNumber { get; }
 }
 
-public class OwnedDependency2 : IOwnedDependency2
+public class OwnedDependency2 : IOwnedDependency2, IDisposable
 {
     private static volatile int _previousInstanceNumber;
 
@@ -14,5 +16,12 @@ public class OwnedDependency2 : IOwnedDependency2
     public OwnedDependency2()
     {
         InstanceNumber = Interlocked.Increment(ref _previousInstanceNumber);
+        Debug.WriteLine($"Created {GetType().Name} instance {InstanceNumber}");
+    }
+
+    public void Dispose()
+    {
+        Debug.WriteLine($"Disposing {GetType().Name} instance {InstanceNumber}");
+        GC.SuppressFinalize(this);
     }
 }
